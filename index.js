5c06c89bcbedb600fee431fd05f310975c6d9aa6
Riley Scheid <riley@tracis.io>
2020-07-10 21:38:28 -0700

    Hot reloading

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const build = require("./build");
+const path = require("path");
 build();
 
 const express = require("express");
-const app = express();
-const open = require("open");
 
+const livereload = require("livereload");
+
+var liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "build/light"));
+
+var connectLivereload = require("connect-livereload");
+
+var app = express();
+
+app.use(connectLivereload());
 app.use(express.static("build/light"));
 app.listen(8085);
 
-open("http://localhost:8085");
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
